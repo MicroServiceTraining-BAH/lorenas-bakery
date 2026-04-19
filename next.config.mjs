06d9e1d6a2d59432c662f7a11e63b28cc430285e
gabriel cajigas <gabrielcajigas@gabriels-Laptop.local>
2026-04-19 15:21:31 -0400

    Revert "chore: migrate from Vercel to Netlify"
    This reverts commit 9c6bd8be0718cc437750d09f9e0f2c2bbb83ea40.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // Vercel Blob storage
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
