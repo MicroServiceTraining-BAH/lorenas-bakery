9c6bd8be0718cc437750d09f9e0f2c2bbb83ea40
gabriel cajigas <gabrielcajigas@gabriels-Laptop.local>
2026-04-19 15:19:10 -0400

    chore: migrate from Vercel to Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        // Vercel Blob storage
-        protocol: 'https',
-        hostname: '*.public.blob.vercel-storage.com',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
